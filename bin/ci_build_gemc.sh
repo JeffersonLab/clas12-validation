472f2bfd6f79a7933b19bdeb769eb8faffa8ee47
Christopher Dilks <c-dilks@users.noreply.github.com>
2023-11-27 13:33:04 -0500

    fix: only the build step should run in the fedora container
    since that container's `tar` does not have `zstd` compression

---
+++ bin/ci_build_gemc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# CI `gemc` build wrapper for CI workflow steps when `uses: [CONTAINER]` is used,
+# which does not seem to be compatible with multi-line commands
+
+if [ $# -ne 1 ]; then
+  echo "USAGE: $0 [source dir]" >&2
+  exit 2
+fi
+pushd $1
+
+source /app/localSetup.sh
+scons -j2 OPT=1
+
+module switch gemc/5.3 # for dependency files ##### FIXME: switch to gemc/dev and use ubuntu + cvmfs action
+
+echo "### GEMC Build Information:"
+./gemc -USE_GUI=0
+
+popd
