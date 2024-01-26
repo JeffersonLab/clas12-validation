cf99456b81dc94f4e3b21d2b043cdf9355b38eb2
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-01-26 16:38:28 -0500

    fix: revert `gemc` changes and reactivate the workflow

--- bin/ci_build_gemc.sh
+++ bin/ci_build_gemc.sh
@@ -11,7 +11,7 @@ pushd $1
 source /app/localSetup.sh
 scons -j4 OPT=1
 
-module switch gemc/5.4 # for dependency files ##### FIXME: switch to gemc/dev and use ubuntu + cvmfs action
+module switch gemc/5.3 # for dependency files ##### FIXME: switch to gemc/dev and use ubuntu + cvmfs action
 
 echo "### GEMC Build Information:"
 ./gemc -USE_GUI=0
