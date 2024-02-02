210b2ffb18d4a78504fc39c775a58b716be44fe0
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-02-02 18:33:23 -0500

    test: module avail

--- bin/ci_gemc_run.sh
+++ bin/ci_gemc_run.sh
@@ -15,6 +15,13 @@ set +u
 source /app/localSetup.sh
 set -u
 
+### show available modules
+echo "=============================="
+echo "MODULE AVAIL"
+echo "=============================="
+module avail --no-pager
+echo "=============================="
+
 ### switch to the gemc module
 ### - if we do not have a custom build, this activates the appropriate gemc version
 ### - if we do have a custom build, this just makes sure the dependencies are resolved correctly
