58fc58c4bb223b0509e559c34dcee3ae59dd9775
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-02-05 14:58:34 -0500

    ci: use highest `clas12Tags` tag and only build GEMC if triggered by `clas12Tags` caller (#69)

---
+++ bin/is_a_tag.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+# check if a given tag is available in a repo; exit 0 if yes, 1 if no
+
+unless ARGV.length == 2
+  $stderr.puts "USAGE: #{$0} [tag] [repo owner]/[repo name]"
+  exit 2
+end
+check_tag, repo = ARGV
+
+api_args = [
+  '--silent',
+  '-L',
+  '-H "Accept: application/vnd.github+json"',
+  '-H "X-GitHub-Api-Version: 2022-11-28"',
+  "https://api.github.com/repos/#{repo}/tags",
+]
+tag_list = `curl #{api_args.join ' '} | jq -r '.[].name'`
+unless $?.success?
+  $stderr.puts "ERROR: failed to get list of tags from #{repo}"
+  exit 1
+end
+
+exit 1 unless tag_list.include? check_tag
